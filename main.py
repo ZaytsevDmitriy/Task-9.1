5522bad0e10a0aeb361141b0f3f7f318b2f068be
Dmitriy Zaytsev <dmitrijzaytsev85@gmail.com>
2021-09-10 18:23:24 +0300

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from pprint import pprint
+Super_hero_list = ['Hulk', 'Captain America', 'Thanos']
 
 class super_hero():
     def __init__(self, name):
@@ -17,23 +18,35 @@ class super_hero():
 
         return intelligence
 
-def comparison_intelligence (name1, name2, name3):
-    super_hero1 = super_hero(name1)
-    super_hero1_intelligence = super_hero1.super_hero_request()
-    super_hero2 = super_hero(name2)
-    super_hero2_intelligence = super_hero2.super_hero_request()
-    super_hero3 = super_hero(name3)
-    super_hero3_intelligence = super_hero3.super_hero_request()
-    if super_hero3_intelligence > super_hero2_intelligence \
-            and  super_hero3_intelligence > super_hero1_intelligence:
-        print((f'Самый умный супергерой - {super_hero3.name}'))
-    elif super_hero2_intelligence > super_hero1_intelligence \
-            and super_hero2_intelligence > super_hero3_intelligence:
-        print((f'Самый умный супергерой - {super_hero2.name}'))
-    else:
-        print((f'Самый умный супергерой - {super_hero1.name}'))
+def comparison_intelligence(list):
+    hero_dict = {}
+    for i in list:
+        super_hero1 = super_hero(i)
+        hero_dict[super_hero1.super_hero_request()] = super_hero1.name
+    cleverest_hero = hero_dict[max(hero_dict.keys())]
 
-comparison_intelligence ('Hulk', 'Captain America', 'Thanos')
+    print((f'Самый умный супергерой - {cleverest_hero}'))
+
+comparison_intelligence(Super_hero_list)
+
+
+# def comparison_intelligence (name1, name2, name3):
+#     super_hero1 = super_hero(name1)
+#     super_hero1_intelligence = super_hero1.super_hero_request()
+#     super_hero2 = super_hero(name2)
+#     super_hero2_intelligence = super_hero2.super_hero_request()
+#     super_hero3 = super_hero(name3)
+#     super_hero3_intelligence = super_hero3.super_hero_request()
+#     if super_hero3_intelligence > super_hero2_intelligence \
+#             and  super_hero3_intelligence > super_hero1_intelligence:
+#         print((f'Самый умный супергерой - {super_hero3.name}'))
+#     elif super_hero2_intelligence > super_hero1_intelligence \
+#             and super_hero2_intelligence > super_hero3_intelligence:
+#         print((f'Самый умный супергерой - {super_hero2.name}'))
+#     else:
+#         print((f'Самый умный супергерой - {super_hero1.name}'))
+
+
 
 
 
